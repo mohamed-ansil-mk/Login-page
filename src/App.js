da4bd780393f046ca22d009f240427c58269f05f
mohamed ansil <mohamedansil007@gmail.com>
2025-10-31 16:37:44 +0530

    Updated login page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <h1 className="title">VISURA🤖</h1>
+      <form className="login-form">
+        <input
+          type="text"
+          placeholder="Username"
+          className="input-field"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input-field"
+          required
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+      <p className="signup-text">
+        Don’t have an account? <a href="#">Sign up</a>
+      </p>
     </div>
   );
 }
